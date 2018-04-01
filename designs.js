a78cf8db4ed949182c96d0a977b1173a417d545b
Brock <brock.marek.it@gmail.com>
2018-04-01 07:35:40 +0000

    v1.2 reducing code

--- designs.js
+++ designs.js
@@ -1,21 +1,16 @@
 //Pixel Project V1.0
-const inputHeight = parseInt($('#inputHeight').val());
-const inputWeight = parseInt($('#inputWeight').val());
-const submitGrid = $('#submitGrid');
 const table = $('#pixelCanvas');
-const colorPicker = $('#colorPicker').val();
 
 // Call make grid on submission.
-submitGrid.on('click', function(event) {
+$('#submitGrid').on('click', function(event) {
 	makeGrid();
 	event.preventDefault();
 });
 
-
 //Display grid based on: inputHeight & inputWeight.
 function makeGrid() {
-	let inputHeight = parseInt($('#inputHeight').val());
-	let inputWeight = parseInt($('#inputWeight').val());
+	const inputHeight = parseInt($('#inputHeight').val());
+	const inputWeight = parseInt($('#inputWeight').val());
 	for (n = 0; n < inputHeight; n++) {
 		$(table).append('<tr></tr>');
 		for (m = 0; m < inputWeight; m++) {
@@ -26,6 +21,6 @@ function makeGrid() {
 
 // Change color on click.
 $(table).delegate('td', 'click', function() {
-	let colorPicker = $('#colorPicker').val();
+	const colorPicker = $('#colorPicker').val();
 	$(this).css('background-color', colorPicker);
 });
